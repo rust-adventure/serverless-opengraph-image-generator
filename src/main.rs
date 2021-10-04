1dd354df8d6a3a9f37282efbded4496db061d1b9
Christopher Biscardi <chris@christopherbiscardi.com>
2021-10-04 16:42:21 -0700

    init opengraph

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use http::StatusCode;
+use lambda_runtime::{handler_fn, Context, Error};
+use og_image_writer::{style, writer::OGImageWriter};
+use serde_json::{json, Value};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let handler_fn = handler_fn(handler);
+    lambda_runtime::run(handler_fn).await?;
+    Ok(())
+}
+
+async fn handler(
+    _: Value,
+    _: Context,
+) -> Result<Value, Error> {
+    let text = "This is Open Graphic Image Writer for Web Developer.";
+
+    let mut writer =
+        OGImageWriter::new(style::WindowStyle {
+            width: 1024,
+            height: 512,
+            background_color: Some(style::Rgba([
+                70, 40, 90, 255,
+            ])),
+            align_items: style::AlignItems::Center,
+            justify_content: style::JustifyContent::Center,
+            ..style::WindowStyle::default()
+        })
+        .unwrap();
+
+    let font =
+        Vec::from(include_bytes!("../FiraSans-Bold.ttf")
+            as &[u8]);
+
+    writer.set_text(
+        text,
+        style::Style {
+            margin: style::Margin(0, 20, 0, 20),
+            line_height: 1.8,
+            font_size: 100.,
+            word_break: style::WordBreak::Normal,
+            color: style::Rgba([255, 255, 255, 255]),
+            text_align: style::TextAlign::Start,
+            ..style::Style::default()
+        },
+        font,
+    )?;
+    let data = writer.into_vec()?;
+
+    let encoded_data = base64::encode(&data);
+    println!("base64 string: {:?}", encoded_data);
+
+    Ok(json!({
+        "headers": {
+            "Content-Type": "image/png",
+            "Content-Length": data.len().to_string()
+        },
+        "statusCode": StatusCode::OK.as_u16(),
+        "body": encoded_data,
+        "isBase64Encoded": true
+    }))
+}
